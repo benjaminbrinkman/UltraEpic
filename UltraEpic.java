21f10cac8076e551020de59c6f64343c6ab3aa23
Benjamin Brinkman <ben@benjaminbrinkman.com>
2016-02-11 11:42:30 -0600

    wrote code to check the output of the testing done with the Test component

--- UltraEpic.java
+++ UltraEpic.java
@@ -2,6 +2,7 @@ public class UltraEpic
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Test");
+		Test testComponent = new Test();
+		System.out.println(testComponent.getClassName());
 	}
 }
