e8f1910d05cd83e68d68003546681c026fd736ff
Benjamin Brinkman <ben@benjaminbrinkman.com>
2016-02-11 12:02:46 -0600

    implemented getContainingType method in abstract Component class

--- Component.java
+++ Component.java
@@ -1,7 +1,19 @@
 public abstract class Component
 {
+	Entity mContainingEntity;
+	
+	public Component(Entity containingEntity)
+	{
+		mContainingEntity = containingEntity;
+	}
+	
 	public String getType()
 	{
 		return this.getClass().getName();
+	}
+
+	public Entity getContainingEntity()
+	{
+		return mContainingEntity;
 	}
 }
